54baf3517ae44ad6fa920922d3517f31ff809af6
Surya-547 <avulasuryaprakash9@gmail.com>
2026-08-06 13:53:00 +0530

    feat: Implemented UserAlreadyExists EXception

---
+++ userservice/src/main/java/com/ordermanagement/userservice/exception/UserAlreadyExistsException.java
@@ -0,0 +1,12 @@
+package com.ordermanagement.userservice.exception;
+
+
+public class UserAlreadyExistsException extends RuntimeException{
+
+    public  UserAlreadyExistsException(String message){
+
+        super(message);
+
+    }
+
+}
