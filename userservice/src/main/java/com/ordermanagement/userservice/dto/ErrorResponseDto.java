983d8d8edae9775d1b93ab8c86445dfc9df7a48b
Surya-547 <avulasuryaprakash9@gmail.com>
2026-09-01 20:47:08 +0530

    feat: Add Validation Exception in ErrorResponseDto

--- userservice/src/main/java/com/ordermanagement/userservice/dto/ErrorResponseDto.java
+++ userservice/src/main/java/com/ordermanagement/userservice/dto/ErrorResponseDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
@@ -19,4 +20,6 @@ public class ErrorResponseDto {
     private String message;
 
     private String path;
+
+    private Map<String, String> validationErrors;
 }
--- userservice/src/main/java/com/ordermanagement/userservice/exception/GlobalExceptionHandler.java
+++ userservice/src/main/java/com/ordermanagement/userservice/exception/GlobalExceptionHandler.java
@@ -4,10 +4,14 @@ package com.ordermanagement.userservice.exception;
 import com.ordermanagement.userservice.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,7 +22,6 @@ public class GlobalExceptionHandler {
         ErrorResponseDto errorResponse = new ErrorResponseDto();
 
         errorResponse.setTimestamp(LocalDateTime.now());
-        errorResponse.setStatus(HttpStatus.CONFLICT.value());
         errorResponse.setError(HttpStatus.CONFLICT.getReasonPhrase());
         errorResponse.setMessage(userAlreadyExistsException.getMessage());
 
@@ -29,5 +32,39 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException userNotFoundException){
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(userNotFoundException.getMessage());
+    }
+
+    @ExceptionHandler(UserIdNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleUserIdNotFoundException(UserIdNotFoundException userIdNotFoundException){
+
+        ErrorResponseDto errorResponse = new ErrorResponseDto();
+
+        errorResponse.setTimestamp(LocalDateTime.now());
+        errorResponse.setError(HttpStatus.NOT_FOUND.getReasonPhrase());
+        errorResponse.setMessage(userIdNotFoundException.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleValidationException(
+            MethodArgumentNotValidException exception) {
+
+        Map<String, String> validationErrors = new HashMap<>();
+
+        ErrorResponseDto errorResponse = new ErrorResponseDto();
+
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            validationErrors.put(
+                    fieldError.getField(),
+                    fieldError.getDefaultMessage()
+            );
+        }
+
+        errorResponse.setValidationErrors(validationErrors);
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
     }
 }
