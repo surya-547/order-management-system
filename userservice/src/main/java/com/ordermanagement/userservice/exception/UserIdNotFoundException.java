7da430b939202429e78ec6c1c7f91b560f4f6958
Surya-547 <avulasuryaprakash9@gmail.com>
2026-09-15 21:34:49 +0530

    feat: Add UserNotFound Exception

---
+++ userservice/src/main/java/com/ordermanagement/userservice/exception/UserIdNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ordermanagement.userservice.exception;
+
+public class UserIdNotFoundException extends RuntimeException{
+
+    public UserIdNotFoundException(String message){
+
+        super(message);
+
+    }
+
+}
